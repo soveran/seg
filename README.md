61d33f7086c8bd2bfb6b179b902409e7521afda3
Michel Martens <michel@soveran.com>
2016-07-08 13:12:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,222 @@
+Seg
+===
+
+Segment matcher for paths.
+
+Description
+-----------
+
+Seg provides two methods for consuming and capturing path segments.
+A path is a string that starts with a slash and contains segments
+separated by slashes, for example `/foo/bar/baz` or `/users/42`.
+
+Usage
+-----
+
+Consider this interactive session:
+
+```crystal
+s = Seg.new("/users/42")
+#=> #<Seg ...>
+
+s.prev
+#=> ""
+
+s.curr
+#=> "/users/42"
+
+s.consume("users")
+#=> true
+
+s.prev
+#=> "/users"
+
+s.curr
+#=> "/42"
+
+s.consume("42")
+#=> true
+
+s.prev
+#=> "/users/42"
+
+s.curr
+#=> ""
+```
+
+The previous example shows how to walk the path by
+providing segments to consume. In the following
+example, we'll see what happens when we try to
+consume a segment with a string that doesn't match:
+
+```crystal
+s = Seg.new("/users/42")
+#=> #<Seg ...>
+
+s.prev
+#=> ""
+
+s.curr
+#=> "/users/42"
+
+s.consume("admin")
+#=> false
+
+s.prev
+#=> ""
+
+s.curr
+#=> "/users/42"
+```
+
+As you can see, the command fails and the `prev` and
+`curr` strings are not altered. Now we'll see
+how to capture segment values:
+
+```crystal
+s = Seg.new("/users/42")
+#=> #<Seg ...>
+
+captures = Hash(Symbol, String).new
+#=> {}
+
+s.prev
+#=> ""
+
+s.curr
+#=> "/users/42"
+
+s.capture(:foo, captures)
+#=> true
+
+s.prev
+#=> "/users"
+
+s.curr
+#=> "/42"
+
+s.capture(:bar, captures)
+#=> true
+
+s.prev
+#=> "/users/42"
+
+s.curr
+#=> ""
+
+captures
+#=> {:foo=>"users", :bar=>42}
+```
+
+It is also possible to `extract` the next segment from the path.
+The method `extract` returns the next segment, if available, or nil
+otherwise:
+
+```crystal
+s = Seg.new("/users/42")
+#=> #<Seg ...>
+
+s.prev
+#=> ""
+
+s.curr
+#=> "/users/42"
+
+s.extract
+#=> "users"
+
+s.prev
+#=> "/users"
+
+s.curr
+#=> "/42"
+
+s.extract
+#=> "42"
+
+s.prev
+#=> "/users/42"
+
+s.curr
+#=> ""
+```
+
+You can also go back by using the methods `retract` and `restore`,
+which are the antidote to `extract` and `consume` respectively.
+
+Let's see how `retract` works:
+
+```crystal
+s = Seg.new("/users/42")
+#=> #<Seg ...>
+
+s.prev
+#=> ""
+
+s.curr
+#=> "/users/42"
+
+s.extract
+#=> "users"
+
+s.prev
+#=> "/users"
+
+s.curr
+#=> "/42"
+
+s.retract
+#=> "users"
+
+s.prev
+#=> ""
+
+s.curr
+#=> "/users/42"
+```
+
+And now `restore`:
+
+```crystal
+s = Seg.new("/users/42")
+#=> #<Seg ...>
+
+s.prev
+#=> ""
+
+s.curr
+#=> "/users/42"
+
+s.extract
+#=> "users"
+
+s.prev
+#=> "/users"
+
+s.curr
+#=> "/42"
+
+s.restore("foo")
+#=> false
+
+s.restore("users")
+#=> true
+
+s.prev
+#=> ""
+
+s.curr
+#=> "/users/42"
+```
+
+Installation
+------------
+
+Add this to your application's `shard.yml`:
+
+```yaml
+dependencies:
+  seg:
+    github: soveran/seg-crystal
+    branch: master
+```
---
+++ spec/seg_spec.cr
@@ -0,0 +1,188 @@
+require "./spec_helper"
+
+describe "nil" do
+  segment = Seg.new(nil)
+
+  assert_equal segment.prev, ""
+  assert_equal segment.curr, "/"
+  assert segment.root?
+end
+
+describe "root" do
+  segment = Seg.new("/")
+
+  assert_equal segment.prev, ""
+  assert_equal segment.curr, "/"
+  assert segment.root?
+end
+
+describe "init" do
+  segment = Seg.new("/")
+
+  assert segment.init?
+  assert segment.root?
+
+  segment = Seg.new("/foo")
+
+  assert segment.init?
+  assert !segment.root?
+
+  assert_equal segment.extract, "foo"
+
+  assert !segment.init?
+  assert segment.root?
+end
+
+describe "extract" do
+  segment = Seg.new("/foo/bar/baz")
+
+  assert_equal segment.extract, "foo"
+  assert_equal segment.prev, "/foo"
+  assert_equal segment.curr, "/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.extract, "bar"
+  assert_equal segment.prev, "/foo/bar"
+  assert_equal segment.curr, "/baz"
+  assert !segment.root?
+
+  assert_equal segment.extract, "baz"
+  assert_equal segment.prev, "/foo/bar/baz"
+  assert_equal segment.curr, ""
+  assert segment.root?
+
+  assert_equal segment.extract, nil
+  assert_equal segment.prev, "/foo/bar/baz"
+  assert_equal segment.curr, ""
+  assert segment.root?
+end
+
+describe "retract" do
+  segment = Seg.new("/foo/bar/baz")
+
+  3.times { segment.extract }
+
+  assert_equal segment.prev, "/foo/bar/baz"
+  assert_equal segment.curr, ""
+
+  assert_equal segment.retract, "baz"
+  assert_equal segment.prev, "/foo/bar"
+  assert_equal segment.curr, "/baz"
+  assert !segment.root?
+
+  assert_equal segment.retract, "bar"
+  assert_equal segment.prev, "/foo"
+  assert_equal segment.curr, "/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.retract, "foo"
+  assert_equal segment.prev, ""
+  assert_equal segment.curr, "/foo/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.retract, nil
+  assert_equal segment.prev, ""
+  assert_equal segment.curr, "/foo/bar/baz"
+  assert !segment.root?
+end
+
+describe "consume" do
+  segment = Seg.new("/foo/bar/baz")
+
+  assert_equal segment.consume("bar"), false
+  assert_equal segment.prev, ""
+  assert_equal segment.curr, "/foo/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.consume("fo"), false
+  assert_equal segment.prev, ""
+  assert_equal segment.curr, "/foo/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.consume("foo"), true
+  assert_equal segment.prev, "/foo"
+  assert_equal segment.curr, "/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.consume("foo"), false
+  assert_equal segment.prev, "/foo"
+  assert_equal segment.curr, "/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.consume("bar"), true
+  assert_equal segment.prev, "/foo/bar"
+  assert_equal segment.curr, "/baz"
+  assert !segment.root?
+
+  assert_equal segment.consume("baz"), true
+  assert_equal segment.prev, "/foo/bar/baz"
+  assert_equal segment.curr, ""
+  assert segment.root?
+
+  assert_equal segment.consume("baz"), false
+  assert_equal segment.prev, "/foo/bar/baz"
+  assert_equal segment.curr, ""
+  assert segment.root?
+end
+
+describe "restore" do
+  segment = Seg.new("/foo/bar/baz")
+
+  3.times { segment.extract }
+
+  assert_equal segment.restore("foo"), false
+  assert_equal segment.prev, "/foo/bar/baz"
+  assert_equal segment.curr, ""
+  assert segment.root?
+
+  assert_equal segment.restore("baz"), true
+  assert_equal segment.prev, "/foo/bar"
+  assert_equal segment.curr, "/baz"
+  assert !segment.root?
+
+  assert_equal segment.restore("bar"), true
+  assert_equal segment.prev, "/foo"
+  assert_equal segment.curr, "/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.restore("foo"), true
+  assert_equal segment.prev, ""
+  assert_equal segment.curr, "/foo/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.restore("foo"), false
+  assert_equal segment.prev, ""
+  assert_equal segment.curr, "/foo/bar/baz"
+  assert !segment.root?
+end
+
+describe "capture" do
+  segment = Seg.new("/foo/bar/baz")
+
+  captures = Hash(Symbol, String).new
+
+  assert_equal segment.capture(:c1, captures), true
+  assert_equal segment.prev, "/foo"
+  assert_equal segment.curr, "/bar/baz"
+  assert !segment.root?
+
+  assert_equal segment.capture(:c2, captures), true
+  assert_equal segment.prev, "/foo/bar"
+  assert_equal segment.curr, "/baz"
+  assert !segment.root?
+
+  assert_equal segment.capture(:c3, captures), true
+  assert_equal segment.prev, "/foo/bar/baz"
+  assert_equal segment.curr, ""
+  assert segment.root?
+
+  assert_equal segment.capture(:c4, captures), false
+  assert_equal segment.prev, "/foo/bar/baz"
+  assert_equal segment.curr, ""
+  assert segment.root?
+
+  assert_equal "foo", captures[:c1]
+  assert_equal "bar", captures[:c2]
+  assert_equal "baz", captures[:c3]
+  assert_equal nil, captures[:c4]?
+end
